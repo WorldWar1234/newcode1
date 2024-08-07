353e634bf77145b552b83d5a5283d78b0a19f6f8
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-07 17:18:31 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -15,22 +15,14 @@ function params(req, res, next) {
       const size = parseInt(headers['content-length'], 10);
       const type = headers['content-type'];
 
-      sharp(url)
-        .metadata()
-        .then(metadata => {
-          req.params.url = url;
-          req.params.originType = type.split('/')[1];
-          req.params.originSize = size;
-          req.params.webp = !jpeg;
-          req.params.grayscale = bw !== '0';
-          req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
+      req.params.url = url;
+      req.params.originType = type.split('/')[1];
+      req.params.originSize = size;
+      req.params.webp = !jpeg;
+      req.params.grayscale = bw !== '0';
+      req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
 
-          next();
-        })
-        .catch(error => {
-          console.error('Error fetching image metadata:', error);
-          res.status(500).send('Error fetching image metadata');
-        });
+      next();
     })
     .catch(error => {
       console.error('Error fetching image headers:', error);
