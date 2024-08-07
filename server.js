578d1a45081f884518bf4994b163f874280c5d1e
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-07 16:28:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 const app = require('express')();
-const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const compress = require('./src/compress');
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/', authenticate, params, compress);
+app.get('/', params, compress);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
